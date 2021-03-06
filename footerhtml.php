351b5075d1b0237ed0eb819768f61ecc5cb00454
Maureen-max <77680352+Maureen-max@users.noreply.github.com>
2021-03-06 09:49:39 +0300

    Add files via upload

---
+++ footerhtml.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>footer php</title>
+</head>
+
+<body>
+    <!-- finding it difficult to run your php code...ensure when you
+     make changes on your code editor you also make changes in your file in htdocs folder-->
+     <!--htdocs is in your xampp folder-->
+    <p>hellow i wanted to try out a php footer code</p><br>
+    <p>i hope it works </p>
+    <?php include "footer.php"?>
+</body>
+
+</html>
